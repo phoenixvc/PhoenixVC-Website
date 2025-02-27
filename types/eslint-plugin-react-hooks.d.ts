8e361c19f8d376e366f6ab2d115cfd2868af2e2a
Jurie Smit <smit.jurie@gmail.com>
2025-02-27 06:14:40 +0000

    feat(types): add new shared type definitions

---
+++ types/eslint-plugin-react-hooks.d.ts
@@ -0,0 +1,9 @@
+declare module 'eslint-plugin-react-hooks' {
+    const plugin: {
+      rules: {
+        'rules-of-hooks': any;
+        'exhaustive-deps': any;
+      };
+    };
+    export default plugin;
+  }
